187c89b6ec10ea0f4742215b298c4cf3f6036217
christophe-g <christophe.geiser@gmail.com>
2020-02-09 14:06:32 +0100

    removed index.class.js

--- index.class.js
+++
@@ -1,4 +0,0 @@
-// import './src/index.js';
-export { default as BlogApp } from  './src/blogApp.js';
-export { default as Posts } from  './src/posts.js';
-export { default as Post} from  './src/post.js';
